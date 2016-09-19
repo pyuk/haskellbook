cddeab0f57b337dd4d7c1b538ac217e149423fb3
pyuk <psi.razputin@gmail.com>
2016-09-18 23:02:15 -0500

    testing stuff

---
+++ ch14/Addition.hs
@@ -0,0 +1,30 @@
+-- Addition.hs
+module Addition where
+
+import Test.Hspec
+
+divideBy :: Integral a => a -> a -> (a, a)
+divideBy num denom = go num denom 0
+  where go n d count
+          | n < d = (count, n)
+          | otherwise = go (n - d) d (count + 1)
+
+multBy :: (Eq a, Num a) => a -> a -> a
+multBy x 0 = 0
+multBy x y = x + multBy x (y - 1)
+
+multByTest :: IO ()
+multByTest = hspec $ do
+  describe "Multiplication" $ do
+    it "9 times 3 is 27" $ do
+      multBy 9 3 `shouldBe` 27
+    it "3 times 7 is 21" $ do
+      multBy 3 7 `shouldBe` 21
+
+main :: IO ()
+main = hspec $ do
+  describe "Addition" $ do
+    it "15 divided by 3 is 5" $ do
+      divideBy 15 3 `shouldBe` (5, 0)
+    it "22 divided by 5 is 4 remainder 2" $ do
+      divideBy 22 5 `shouldBe` (4, 2)

670a91f7efe9dce0b16c4b2a682037685dae8595
pyuk <psi.razputin@gmail.com>
2016-09-22 23:50:16 -0500

    chch15

--- ch15.hs
+++ ch15.hs
@@ -1,15 +1,18 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 import Data.Monoid hiding ((<>))
-import Test.QuickCheck
+import Test.QuickCheck hiding (Failure, Success)
 import Data.Semigroup
+import GHC.Generics
 
---monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool
---monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+monoidAssoc :: (Semigroup m, Eq m, Monoid m) => m -> m -> m -> Bool
+monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
 
---monoidLeftIdentity :: (Eq m, Monoid m) => m -> Bool
---monoidLeftIdentity a = (mempty <> a) == a
---
---monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
---monoidRightIdentity a = (a <> mempty) == a
+monoidLeftIdentity :: (Semigroup m, Eq m, Monoid m) => m -> Bool
+monoidLeftIdentity a = (mempty <> a) == a
+
+monoidRightIdentity :: (Semigroup m, Eq m, Monoid m) => m -> Bool
+monoidRightIdentity a = (a <> mempty) == a
 
 data Optional a = Nada | Only a deriving (Eq, Show)
 
@@ -64,6 +67,10 @@ newtype Identity a = Identity a deriving (Eq, Show)
 instance Semigroup a => Semigroup (Identity a) where
   Identity a <> Identity b = Identity (a <> b)
 
+--instance Semigroup a => Monoid (Identity a) where
+--  mempty = Identity {}
+--  mappend = (<>)
+
 instance Arbitrary a => Arbitrary (Identity a) where
   arbitrary = do
     a <- arbitrary
@@ -71,7 +78,152 @@ instance Arbitrary a => Arbitrary (Identity a) where
 
 type IdentityAssoc = Identity String -> Identity String -> Identity String -> Bool
 
+data Two a b = Two a b deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
+  (Two x y) <> (Two m n) = Two (x <> m) (y <> n)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    return $ Two a b
+
+type TwoAssoc = Two String String -> Two String String -> Two String String -> Bool
+
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b, Semigroup c) => Semigroup (Three a b c) where
+  (Three x y z) <> (Three a b c) = Three (x <> a) (y <> b) (z <> c)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return $ Three a b c
+
+type ThreeAssoc =
+     Three String String String
+  -> Three String String String
+  -> Three String String String
+  -> Bool
+
+data Four a b c d = Four a b c d deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b, Semigroup c, Semigroup d) =>
+  Semigroup (Four a b c d) where
+  (Four a b c d) <> (Four a' b' c' d') = Four (a <> a') (b <> b') (c <> c') (d <> d')
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) =>
+  Arbitrary (Four a b c d) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    d <- arbitrary
+    return $ Four a b c d
+
+type FourAssoc =
+     Four String String String String
+  -> Four String String String String
+  -> Four String String String String
+  -> Bool
+
+data BoolConj = BoolConj Bool deriving (Eq, Show)
+
+instance Semigroup BoolConj where
+  BoolConj True <> BoolConj True = BoolConj True
+  BoolConj _ <> BoolConj _ = BoolConj False
+
+instance Arbitrary BoolConj where
+  arbitrary = frequency [(1,return $ BoolConj True), (1,return $ BoolConj False)]
+
+type BoolConjAssoc = BoolConj -> BoolConj -> BoolConj -> Bool
+
+data BoolDisj = BoolDisj Bool deriving (Eq, Show)
+
+instance Semigroup BoolDisj where
+  (BoolDisj False) <> (BoolDisj False) = BoolDisj False
+  _ <> _ = BoolDisj True
+
+instance Arbitrary BoolDisj where
+  arbitrary = frequency [(1,return $ BoolDisj True), (1,return $ BoolDisj False)]
+
+type BoolDisjAssoc = BoolDisj -> BoolDisj -> BoolDisj -> Bool
+
+data Or a b = Fst a | Snd b deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (Or a b) where
+  (Snd x) <> _ = Snd x
+  _ <> (Snd x) = Snd x
+  _ <> x = x
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Or a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    frequency [(1,return $ Fst a), (1,return $ Snd b)]
+
+type OrAssoc = Or String String -> Or String String -> Or String String -> Bool
+
+newtype Combine a b = Combine { unCombine :: (a -> b) } deriving Generic
+
+instance Semigroup b => Semigroup (Combine a b) where
+  (Combine f) <> (Combine g) = Combine $ \n -> f n <> g n
+
+instance (CoArbitrary a, Arbitrary b) => Arbitrary (Combine a b) where
+  arbitrary = do
+    x <- coarbitrary (Combine $ \n -> 'b' : n) arbitrary
+    return $ Combine x
+
+instance (Arbitrary a, CoArbitrary b) => CoArbitrary (Combine a b)
+
+type CombineAssoc = Combine String String -> Combine String String -> Combine String String -> Bool
+
+newtype Comp a = Comp { unComp :: (a -> a) }
+
+instance Semigroup a => Semigroup (Comp a) where
+  Comp f <> Comp g = Comp $ f . g
+
+data Validation a b = Failure a | Success b deriving (Eq, Show)
+
+instance Semigroup a => Semigroup (Validation a b) where
+  (<>) (Failure x) (Failure y) = Failure (x <> y)
+  (<>) (Success x) (Failure _) = Success x
+  (<>) (Failure _) (Success x) = Success x
+  (<>) (Success x) (Success _) = Success x
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Validation a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    frequency [(1,return $ Failure a), (1,return $ Success b)]
+
+type ValidationAssoc = Validation String String -> Validation String String -> Validation String String -> Bool
+
+newtype AccumulateRight a b = AccumulateRight (Validation a b) deriving (Eq, Show)
+
+instance Semigroup b => Semigroup (AccumulateRight a b) where
+  (<>) (AccumulateRight (Success x)) (AccumulateRight (Success y)) = AccumulateRight (Success $ x <> y)
+  (<>) (AccumulateRight (Failure x)) (AccumulateRight (Success y)) = AccumulateRight (Success y)
+  (<>) (AccumulateRight (Success x)) (AccumulateRight (Failure y)) = AccumulateRight (Success x)
+  (<>) x _ = x
+
+newtype AccumulateBoth a b = AccumulateBoth (Validation a b) deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (AccumulateBoth a b) where
+  (<>) (AccumulateBoth x) (AccumulateBoth y) = AccumulateBoth (x <> y)
+
+
 main :: IO ()
 main = do
   quickCheck (semigroupAssoc :: TrivialAssoc)
   quickCheck (semigroupAssoc :: IdentityAssoc)
+  quickCheck (semigroupAssoc :: TwoAssoc)
+  quickCheck (semigroupAssoc :: ThreeAssoc)
+  quickCheck (semigroupAssoc :: FourAssoc)
+  quickCheck (semigroupAssoc :: BoolConjAssoc)
+  quickCheck (semigroupAssoc :: BoolDisjAssoc)
+  quickCheck (semigroupAssoc :: OrAssoc)
+  quickCheck (semigroupAssoc :: ValidationAssoc)

b6eae1ed4568c2144ccc4537868e57d0318814db
pyuk <psi.razputin@gmail.com>
2016-10-02 01:14:23 -0500

    22

--- ch22.hs
+++ ch22.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE InstanceSigs #-}
 
+import Control.Applicative
+import Data.Maybe
 import Data.Char
 
 cap :: [Char] -> [Char]
@@ -83,3 +85,43 @@ chris :: Person
 chris = Person (HumanName "Chris Allen")
         (DogName "Papu")
         (Address "Austin")
+
+x = [1,2,3]
+y = [4,5,6]
+z = [7,8,9]
+
+xs :: Maybe Integer
+xs = lookup 3 $ zip x y
+
+ys :: Maybe Integer
+ys = lookup 6 $ zip y z
+
+zs :: Maybe Integer
+zs = lookup 4 $ zip x y
+
+z' :: Integer -> Maybe Integer
+z' n = lookup n $ zip x z
+
+x1 :: Maybe (Integer, Integer)
+x1 = (,) <$> xs <*> ys
+
+x2 :: Maybe (Integer, Integer)
+x2 = (,) <$> ys <*> zs
+
+x3 :: Integer -> (Maybe Integer, Maybe Integer)
+x3 n = (,) (z' n) (z' n)
+
+summed :: Num c => (c, c) -> c
+summed = uncurry (+)
+
+bolt :: Integer -> Bool
+bolt = (&&) <$> (>3) <*> (<8)
+
+sequA :: Integral a => a -> [Bool]
+sequA m = sequenceA [(>3), (<8), even] m
+
+main :: IO ()
+main = do
+  print $ foldr (&&) True $ sequA 5
+  print $ sequA (summed $ fromMaybe (0,0) x1)
+  print $ bolt (fromMaybe 0 ys)

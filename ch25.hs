b0754d9fb35482e7ac7db646ac04d9826f10ce3f
pyuk <psi.razputin@gmail.com>
2016-10-15 21:01:30 -0500

    bifunctor

--- ch25.hs
+++ ch25.hs
@@ -1,3 +1,4 @@
+
 newtype Compose f g a = Compose { getCompose :: f (g a) }
   deriving (Eq, Show)
 
@@ -7,3 +8,51 @@ instance (Functor f, Functor g) => Functor (Compose f g) where
 instance (Applicative f, Applicative g) => Applicative (Compose f g) where
   pure a = Compose $ pure (pure a)
   (Compose f) <*> (Compose a) = Compose $ (<*>) <$> f <*> a
+
+instance (Foldable f, Foldable g) => Foldable (Compose f g) where
+  foldMap f (Compose x) = (foldMap . foldMap) f x
+
+instance (Traversable f, Traversable g) => Traversable (Compose f g) where
+  traverse f (Compose x) = Compose <$> (traverse . traverse) f x
+
+class Bifunctor p where
+  {-# MINIMAL bimap | first, second #-}
+  bimap :: (a -> b) -> (c -> d) -> p a c -> p b d
+  bimap f g = first f . second g
+
+  first :: (a -> b) -> p a c -> p b c
+  first f = bimap f id
+
+  second :: (b -> c) -> p a b -> p a c
+  second = bimap id
+
+data Deux a b = Deux a b
+
+instance Bifunctor Deux where
+  bimap f g (Deux a b) = Deux (f a) (g b)
+
+data Const a b = Const a
+
+instance Bifunctor Const where
+  bimap f g (Const x) = Const (f x)
+
+data Drei a b c = Drei a b c
+
+instance Bifunctor (Drei a) where
+  bimap f g (Drei a b c) = Drei a (f b) (g c)
+
+data SuperDrei a b c = SuperDrei a b
+
+instance Bifunctor (SuperDrei a) where
+  bimap f g (SuperDrei a b) = SuperDrei a (f b)
+
+data Quadriceps a b c d = Quadzzz a b c d
+
+instance Bifunctor (Quadriceps a b) where
+  bimap f g (Quadzzz a b c d) = Quadzzz a b (f c) (g d)
+
+data Either' a b = Left' a | Right' b
+
+instance Bifunctor Either' where
+  bimap f g (Left' a) = Left' $ f a
+  bimap f g (Right' b) = Right' $ g b

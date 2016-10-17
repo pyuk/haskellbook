b2935b8a572811eee33fc811f08438de4c820773
pyuk <psi.razputin@gmail.com>
2016-10-17 00:10:12 -0500

    EitherT

--- ch26.hs
+++ ch26.hs
@@ -6,3 +6,25 @@ instance Functor m => Functor (EitherT e m) where
 instance Applicative m => Applicative (EitherT e m) where
   pure x = EitherT $ pure (pure x)
   EitherT meab <*> EitherT mea = EitherT $ (<*>) <$> meab <*> mea
+
+instance Monad m => Monad (EitherT e m) where
+  return = pure
+  EitherT mea >>= f = EitherT $ do
+    v <- mea
+    case v of
+      Left x -> return $ Left x
+      Right x -> runEitherT $ f x
+
+swapEither :: Either e a -> Either a e
+swapEither (Left x) = Right x
+swapEither (Right x) = Left x
+
+swapEitherT :: Functor m => EitherT e m a -> EitherT a m e
+swapEitherT (EitherT mea) = EitherT $ fmap swapEither mea
+
+eitherT :: Monad m => (a -> m c) -> (b -> m c) -> EitherT a m b -> m c
+eitherT f g (EitherT mea) = do
+  v <- mea
+  case v of
+    Left x -> f x
+    Right x -> g x

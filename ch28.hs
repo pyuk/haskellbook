20f38be42ef925de666edde6216dd4e5e89662fa
pyuk <psi.razputin@gmail.com>
2016-11-01 00:06:27 -0500

    slowly doing something

--- ch28.hs
+++ ch28.hs
@@ -2,5 +2,17 @@ import Criterion.Main
 import qualified Data.Map as M
 import qualified Data.Set as S
 
+bumpIt :: (Int, Int) -> (Int, Int)
+bumpIt (i, v) = (i + 1, v + 1)
+
 m :: M.Map Int Int
-m = 
+m = M.fromList $ take 10000 stream
+  where stream = iterate bumpIt (0, 0)
+
+m2 = m
+
+s :: S.Set Int
+s = S.fromList $ take 10000 stream
+  where stream = iterate (+1) 0
+
+s2 = s

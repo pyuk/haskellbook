ec1aa5cc1addd5071486283d79892d9da97123dd
pyuk <psi.razputin@gmail.com>
2016-11-12 23:58:39 -0600

    messing with io

--- ch29/vigIO/.stack-work/intero/intero5723HCS.hs
+++ ch29/vigIO/.stack-work/intero/intero5723HCS.hs
@@ -3,5 +3,11 @@ module Main where
 import VigCipher
 import Lib
 
+vigIO :: String -> String -> IO ()
+vigIO arg key = print $ vigCipher arg key
+
 main :: IO ()
-main = someFunc
+main = do
+  arg <- getLine
+  key <- getLine
+  vigIO arg key
